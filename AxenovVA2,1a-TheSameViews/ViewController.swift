c0fae576d1826d964788de082970002e12d511d2
Vitaly Axenov <hrmobs@gmail.com>
2022-01-09 22:11:34 +0300

    Initial Commit

---
+++ AxenovVA2,1a-TheSameViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AxenovVA2,1a-TheSameViews
+//
+//  Created by Виталий Аксенов on 09.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
